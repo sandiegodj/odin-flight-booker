7524a700015ca5691052a294c4a9a02b42a4b4f8
sandiegodj <davidjanczyn@gmail.com>
2014-10-15 17:11:06 -0700

    fix n + 1 query

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -14,7 +14,7 @@ class Flight < ActiveRecord::Base
       date = params[:date].to_date
       Flight.where(departure_id: params[:from_airport], arrival_id: params[:to_airport],
                   date: date.beginning_of_day..date.end_of_day)
-                #this includes is for n + 1 queries
+                  .includes(:from_airport, :to_airport)
     end
  end
 
